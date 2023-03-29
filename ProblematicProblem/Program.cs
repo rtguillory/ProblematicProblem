274d91127870cbc6a4b69cb13df80cb03205d2ad
Rhonda Guillory <guilloryrt.ps@gmail.com>
2023-03-28 22:43:41 -0500

    edits based on comments

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -10,6 +10,7 @@ namespace ProblematicProblem
         
         static bool cont = true;
         static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
+        //static List<string> activities = new List<string>() { "Movies", "Paintball", "Wine Tasting" };  //shorter list for testing
         static void Main(string[] args)
         {   
             Random rng = new Random();
@@ -17,8 +18,13 @@ namespace ProblematicProblem
             //var contResponse = Console.ReadLine().ToLower();
             cont = (Console.ReadLine().ToLower() == "yes") ? true : false;
 
+            if(!cont)
+                Console.WriteLine("Come back when you are ready to get an activity!");
             
-                 Console.WriteLine();
+            while (cont)
+            {
+
+                Console.WriteLine();
 
                 Console.Write("We are going to need your information first! What is your name? ");
                 string userName = Console.ReadLine();
@@ -27,6 +33,7 @@ namespace ProblematicProblem
                 Console.Write("What is your age? ");
                 int userAge = int.Parse(Console.ReadLine());
                 Console.WriteLine();
+                if (userAge < 21) { activities.Remove("Wine Tasting"); }
 
                 Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
                 bool seeList = (Console.ReadLine().ToLower() == "sure") ? true : false;
@@ -60,8 +67,8 @@ namespace ProblematicProblem
                         addToList = (Console.ReadLine().ToLower() == "yes") ? true : false;
                     }
                 }
-
-                while (cont)
+                bool cont2 = true;
+                while (cont2)
                 {
                     Console.Write("Connecting to the database");
                     for (int i = 0; i < 10; i++)
@@ -79,19 +86,47 @@ namespace ProblematicProblem
                     Console.WriteLine();
                     int randomNumber = rng.Next(activities.Count);
                     string randomActivity = activities[randomNumber];
-
-                    if (userAge < 21 && randomActivity == "Wine Tasting")
-                    {
-                        Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
-                        Console.WriteLine("Picking something else!");
-                        activities.Remove(randomActivity);
-                        randomNumber = rng.Next(activities.Count);
-                        randomActivity = activities[randomNumber];
-                    }
+                        //removed wine tasting above if age was less than 21 so this is no longer needed
+                    //if (userAge < 21 && randomActivity == "Wine Tasting")
+                    //{
+                    //    Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
+                    //    Console.WriteLine("Picking something else!");
+                    //    activities.Remove(randomActivity);
+                     //        randomNumber = rng.Next(activities.Count);
+                    //        randomActivity = activities[randomNumber];
+                    //    } 
+                    //}
                     Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
                     Console.WriteLine();
-                    cont = (Console.ReadLine().ToLower() == "redo") ? true : false;
+                    cont2 = (Console.ReadLine().ToLower() == "redo") ? true : false;
+
+                    if (!cont2)
+                    {
+                        Console.WriteLine("Enjoy your activity!");
+                        cont = false;
+                    }
+                    else 
+                    {
+                        //Remove activities so duplicates do not show up when redoing
+                        if (activities.Count > 1 )
+                        {
+                            activities.Remove(randomActivity);
+                        }
+                        else
+                        {
+                            //stop when all activities have been offered
+                            Console.WriteLine("There are no further activities.  Please begin again.");
+                            cont = false;
+                            cont2 = false;
+                        }
+                            
+                    }
+                    
                 }
+            }
+               
+            Console.ReadLine();
+
             
         }
     }
